11b52817ea617b0dbb433e7202999dadeaaf5037
Mark Johnson <markjohnson303@gmail.com>
2016-07-06 22:21:15 -0600

    all routes working

--- starter_code/server.js
+++ starter_code/server.js
@@ -1,6 +1,9 @@
 'use strict'
-var express = require('express')
-var app = express() 
+var express = require('express');
+var app = express() ;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 var candies = [
   {
@@ -20,5 +23,52 @@ var candies = [
   }
 ];
 
-app.listen(3000)
-console.log("Server listening on port 3000")
+var nextID = 4;
+
+app.get('/candies/:id', function(req, res){
+  var foundCandy;
+  var targetID = parseInt(req.params.id);
+  for (var i=0; i<candies.length; i++){
+    if (candies[i].id === targetID){
+        foundCandy = candies[i];
+}
+  }
+  res.send(foundCandy);
+});
+
+app.get('/candies/', function(req, res){
+  res.send(candies);
+});
+
+app.post('/candies', function(req, res){
+  var newCandy= req.body;
+  if (newCandy.id === undefined){
+    newCandy.id = nextID;
+    nextID++;
+  }
+  candies.push(newCandy);
+  console.log(newCandy);
+  res.send(newCandy);
+});
+
+app.put('/candies/:id', function(req, res){
+  var newCandy= req.body;
+  newCandy.id = parseInt(req.params.id);
+  candies.push(newCandy);
+  console.log(newCandy);
+  res.send(newCandy);
+});
+
+app.delete('/candies/:id', function(req, res){
+  var deleteCandyID = parseInt(req.params.id);
+  for (var i=0; i<candies.length; i++){
+    if (candies[i].id === deleteCandyID){
+        var foundCandy = candies[i];
+        candies.splice(i, 1);
+}
+  res.send("candy " + req.params.id + " deleted");
+}
+});
+
+app.listen(3000);
+console.log("Server listening on port 3000");
